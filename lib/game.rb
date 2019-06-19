ec0ac76272e7411acd8eeb2f22cf73a6bfbe2a4f
Sone Gillis <sone_gillis@yahoo.com>
2019-06-19 23:13:43 +0100

    Implement naked classes and methods

---
+++ lib/game.rb
@@ -0,0 +1,33 @@
+class Game
+  def initialize
+  end
+
+  def start
+  end
+
+  @private
+  class Board
+    def initialize
+    end
+
+    def display
+    end
+
+    def full?
+    end
+  end
+
+  class Player
+    def initialize
+    end
+
+    def play
+    end
+  end
+
+  def settings
+  end
+
+  def check_win(mark)
+  end
+end
